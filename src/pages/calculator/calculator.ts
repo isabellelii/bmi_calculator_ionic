0883a113b592646831f6cd92224006f309dfaa31
Isabelle <isabellelidberg@live.se>
2018-03-06 11:55:22 +0100

    Added calcutation for weight and height added variables for weight and height Declared function calculateBMI

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the CalculatorPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-calculator',
   templateUrl: 'calculator.html',
 })
 export class CalculatorPage {
+  height: number;
+  weight: number;
+  bmiMessage: string; 
+  bmiValue: number; 
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -22,4 +19,29 @@ export class CalculatorPage {
     console.log('ionViewDidLoad CalculatorPage');
   }
 
+  calculateBMI() {
+    if (this.weight > 0 && this.height > 0) {
+      let finalBmi = this.weight / (this.height / 100 * this.height / 100); 
+      this.bmiValue = parseFloat(finalBmi.toFixed(2)); 
+      this.setBMIMessage(); 
+    }
+  }
+
+  private setBMIMessage() {
+    if (this.bmiValue < 18.5) {
+      this.bmiMessage = "Underweight"
+    }
+
+    if (this.bmiValue > 18.5 && this.bmiValue < 25) {
+      this.bmiMessage = "Normal"
+    }
+
+    if (this.bmiValue > 25 && this.bmiValue < 30) {
+      this.bmiMessage = "Overweight"
+    }
+
+    if (this.bmiValue > 30) {
+      this.bmiMessage = "Obese"
+    }
+  }
 }
